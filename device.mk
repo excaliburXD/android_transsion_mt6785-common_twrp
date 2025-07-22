2dfc1e88258afcecc55b410257c6d1d29567a106
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2025-07-22 18:48:47 +0700

    mt6785-Common: using base pre
    Signed-off-by: Gilang EA. <144137514+excaliburXD@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
