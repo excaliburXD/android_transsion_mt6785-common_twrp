bcfdaeaf84a40a2a64997efb660ba590ac78db69
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2025-07-22 18:52:19 +0700

    mt6785-Common: add Additional Libraries
    - libkeymaster4
    - libpuresoftkeymasterdevice
    
    Signed-off-by: Gilang EA. <144137514+excaliburXD@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -76,3 +76,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
+
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
